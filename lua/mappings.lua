d63e0dcfb98b026d83df3ea1343c04f4c46006b0
FukeKazki <kazkichi0906@gmail.com>
2024-06-23 14:10:57 +0900

    :+1: 使いやすく

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,7 +2,6 @@ return {
   t = {
     ["jj"] = { "<C-\\><C-n>" },
     ["<ESC>"] = { "<C-\\><C-n>" },
-    -- terminal モード中の画面移動
     ["<C-k>"] = {
       function() require("smart-splits").move_cursor_up() end,
     },
@@ -17,7 +16,23 @@ return {
     },
   },
   n = {
-    ["|"] = { "<cmd>vnew<cr><cmd>terminal<cr>", desc = "Vertical Split" },
-    ["\\"] = { "<cmd>new<cr><cmd>terminal<cr>", desc = "Horizontal Split" },
+    ["|"] = { "<cmd>vnew<cr>", desc = "Vertical Split" },
+    ["\\"] = { "<cmd>new<cr>", desc = "Horizontal Split" },
+  },
+  i = {
+    ["jj"] = { "<C-\\><C-n>" },
+    ["<ESC>"] = { "<C-\\><C-n>" },
+    ["<C-k>"] = {
+      function() require("smart-splits").move_cursor_up() end,
+    },
+    ["<C-j>"] = {
+      function() require("smart-splits").move_cursor_down() end,
+    },
+    ["<C-h>"] = {
+      function() require("smart-splits").move_cursor_left() end,
+    },
+    ["<C-l>"] = {
+      function() require("smart-splits").move_cursor_right() end,
+    },
   },
 }
