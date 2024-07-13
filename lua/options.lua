cd6f2d4ed1a086a07890c56a9fed479b14f1e5a9
FukeKazki <kazkichi0906@gmail.com>
2024-07-13 12:24:30 +0900

    ♻️ refactor

---
+++ lua/options.lua
@@ -0,0 +1,13 @@
+vim.opt.title = true
+vim.opt.swapfile = false
+-- 行番号を表示
+vim.opt.number = true
+-- 相対行を表示
+vim.opt.relativenumber = true
+-- system の clipboard を使う
+vim.opt.clipboard = "unnamedplus"
+-- ステータスラインを一番下に固定
+vim.opt.laststatus = 3
+vim.opt.cmdheight = 0
+-- fold
+vim.opt.foldmethod = "manual"
